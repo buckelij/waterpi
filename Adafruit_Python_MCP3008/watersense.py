266deb8c39c90d3d8fffac7f99085e0ca6518ab4
waterpi <pi@waterpi.ideasyncratic.net>
2019-11-19 21:05:45 +0000

    reinit

---
+++ Adafruit_Python_MCP3008/watersense.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Simple example of reading the MCP3008 analog input channels and printing
+# them all out.
+# Author: Tony DiCola
+# License: Public Domain
+import time
+
+# Import SPI library (for hardware SPI) and MCP3008 library.
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
+
+
+# Software SPI configuration:
+# 6, 13, 19, 26 == clk, dout, din, cs/shd
+CLK  = 6
+MISO = 13
+MOSI = 19
+CS   = 26
+mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
+
+# Hardware SPI configuration:
+# SPI_PORT   = 0
+# SPI_DEVICE = 0
+# mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
+
+v = mcp.read_adc(0)
+print('{0:>4}'.format(v))
